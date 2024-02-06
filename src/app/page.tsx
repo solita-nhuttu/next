c4a8ceb40f9e1771a98f6010abea7ccaa908a2a7
Niko Huttu <niko.huttu@solita.fi>
2024-02-06 17:50:02 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
 export default function Page() {
   async function create(formData: FormData) {
-    'use server';
+    "use server";
     console.log(formData);
   }
- 
+
   return (
     <div>
-
-    <form action={create}>
-      <input type="text" name="name" />
-      <button className="flex justify-normal" type="submit">Submit</button>
-      <div>
-      pitäs toimia
-      Hei! {process.env.NEXT_PUBLIC_TESTATAAN ?? 'Ei olla azuressa'}
-      </div>
-    </form>
+      <form action={create}>
+        <input type="text" name="name" />
+        <button className="flex justify-normal" type="submit">
+          Submit
+        </button>
+        <div>
+          pitäs toimia A11111!!! Hei!{" "}
+          {process.env.NEXT_PUBLIC_TESTATAAN ?? "Ei olla azuressa"}
+        </div>
+      </form>
     </div>
   );
 }
