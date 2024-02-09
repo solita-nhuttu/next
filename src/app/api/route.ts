0609c67fbcb209322c01d35f90f1de0bcd178635
Niko Huttu <niko.huttu@solita.fi>
2024-02-09 14:26:42 +0200

    .env

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 export async function GET(request: Request) {
   return new Response(
     `Hello, ${request.url}!, is process defined or not? ${process.env.AZURE_CONNECTION_STRING?.substring(0, 7) ?? "It was not"} and then length ${process.env.AZURE_CONNECTION_STRING?.length} and test env: ${process.env.TEST_ENV}`,
